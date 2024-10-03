6dedc2906c4fd81bcecdbb8157067dce047763ce
Pankaj Kumar Dhal <pk99dhal@gmail.com>
2024-10-03 20:58:35 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "../src/page/navbar";
 import Main from "../src/page/main";
 import Video from "../src/page/video";
 import Footer from "../src/page/footer";
-import Home from "../src/components/Home";
+import Home from "../src/components/home";
 import About from "../src/components/about";
 import Contact from "../src/components/contact";
 import Pricing from "../src/components/pricing";
